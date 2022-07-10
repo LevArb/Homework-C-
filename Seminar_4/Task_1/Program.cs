d69593ad267082bb102cf5d2b763abdc72d03ef6
Lev <vasiliybubka@mail.ru>
2022-07-10 13:06:16 +0300

    Made tasks from seminar 4

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243
+2, 4 -> 16
+*/
+try
+{
+    Console.Clear();
+    Console.WriteLine("Цикл, возводящий в степень, вместо Pow()");
+
+    Console.Write("Введите число: ");
+    string inputX = Console.ReadLine();
+    int x = int.Parse(inputX);
+
+    Console.Write("Введите число степени возведения: ");
+    string inputY = Console.ReadLine();
+    int y = int.Parse(inputY);
+
+    int fX = x;
+    for (int i = 1; i < y; i++)
+    {
+        x = x * fX;
+    }
+    Console.WriteLine($"Ответ: {x}");
+}
+catch (System.FormatException)
+{
+    Console.Clear();
+    Console.WriteLine("Вы ввели не число, введите корректные данные");
+    
+}
---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+try
+{
+Console.Clear();
+Console.WriteLine("Суммирую все числа введенного числа и вывожу ответ");
+Console.Write("Ведите число: ");
+string input = Console.ReadLine();
+long x = long.Parse(input);
+long ten = 10;
+long y = 1;
+long i = 1;
+long answer = 0;
+
+while (i <= input.Length)
+{
+    answer = answer + (x / y) % 10;
+    i++;
+    y = y * ten;
+}
+
+Console.WriteLine($"Сумма цифр вашего числа: {answer}");
+}
+catch (System.FormatException)
+{
+    Console.Clear();
+    Console.WriteLine("Вы ввели не число, введите корректное данные");
+}
