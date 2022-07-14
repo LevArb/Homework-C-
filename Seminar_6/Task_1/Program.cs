823c9ec190701af2a65eaaf60a94d37d02c547e6
Lev <vasiliybubka@mail.ru>
2022-07-14 23:56:18 +0300

    Made tasks from seminar 6

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int inpt = 0;
+int count = 0;
+Console.Clear();
+Console.WriteLine(string.Empty);
+Console.WriteLine("Вводите числа по очереди через 'Enter' и я посчитаю сколько из них больше нуля.");
+Console.WriteLine("Введите всё кроме числа для вывода результатов и завершения программы");
+while (true)
+{
+    Console.Write("Введите число:");
+    if (int.TryParse(Console.ReadLine(), out inpt))
+    {
+        if (inpt > 0)
+        {
+            count++;
+        }
+    }
+    else
+    {
+        Console.WriteLine(string.Empty);
+        Console.WriteLine($"Чисел больше 0 введено: {count}");
+        Console.WriteLine(string.Empty);
+        break;
+    }
+}
+return inpt;
---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+Console.Clear();
+Console.WriteLine(string.Empty);
+Console.WriteLine("Нахожу точку пересечения двух прямых по заданным вами координатам");
+
+Console.Write("Введите координату k1: ");
+string inpt1 = Console.ReadLine();
+double k1 = double.Parse(inpt1);
+
+Console.Write("Введите координату k2: ");
+string inpt2 = Console.ReadLine();
+double k2 = double.Parse(inpt2);
+
+Console.Write("Введите координату b1: ");
+string inpt3 = Console.ReadLine();
+double b1 = double.Parse(inpt3);
+
+Console.Write("Введите координату b2: ");
+string inpt4 = Console.ReadLine();
+double b2 = double.Parse(inpt4);
+
+double x = (b2 - b1)/(k1 - k2);
+
+double point1 = k1 * x + b1;
+double point2 = k2 * x + b2;
+
+Console.WriteLine(string.Empty);
+Console.WriteLine($"Точка пересечения прямых: {point1}; {point2}");
